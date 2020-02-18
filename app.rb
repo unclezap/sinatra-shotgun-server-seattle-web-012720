509ce94766b630d904551f7e41d57452391efdd4
unclezap <zeb.blackwell@gmail.com>
2020-02-18 00:12:40 -0800

    would like to be able to submit work here

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! LOL we're useless without learn specs testing shotgun now"
   end
 
 end
